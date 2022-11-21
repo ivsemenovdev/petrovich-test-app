544903866e91364f21917bb51114ed2b9ef9fe4d
Ivan Semenov <ivsemenovdev@gmail.com>
2022-11-21 21:52:45 +0300

    feat: add home-page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainLayoutComponent
+    MainLayoutComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
