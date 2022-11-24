d55db2d6e235ea3815138e2716f51b39ce0f19cf
Ivan Semenov <ivsemenovdev@gmail.com>
2022-11-25 00:28:55 +0300

    feat: add header link interface

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,21 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {SiteLayoutComponent} from "./layouts/site-layout/site-layout.component";
 
 const routes: Routes = [
   {
     path: '', component: SiteLayoutComponent, children: [
       {path: '', redirectTo: '/', pathMatch: 'full'},
-    ]
-  }
+    ],
+  },
+  {
+    path: '**', component: SiteLayoutComponent
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/blocks/header/components/header-top/header-top.component.ts
+++ src/app/blocks/header/components/header-top/header-top.component.ts
@@ -1,17 +1,19 @@
 import {Component} from '@angular/core';
+import {HeaderLinkInterface} from "../../header-link.interface";
 
 @Component({
   selector: 'app-header-top',
   templateUrl: './header-top.component.html',
   styleUrls: ['./header-top.component.scss']
 })
+
 export class HeaderTopComponent {
-  menuItems = [
-    {item: 'Пункты самовывоза'},
-    {item: 'Доставка'},
-    {item: 'Самовывоз'},
-    {item: 'Акции'},
-    {item: 'Клуб друзей'},
-    {item: 'Услуги'},
+  menuItems: HeaderLinkInterface[] = [
+    {item: 'Пункты самовывоза', route: '/contacts/map-bases/'},
+    {item: 'Доставка', route: '/services/delivery/'},
+    {item: 'Самовывоз', route: '/buyers/return/'},
+    {item: 'Акции', route: '/promos/'},
+    {item: 'Клуб друзей', route: '#'},
+    {item: 'Услуги', route: '/services/'},
   ];
 }
