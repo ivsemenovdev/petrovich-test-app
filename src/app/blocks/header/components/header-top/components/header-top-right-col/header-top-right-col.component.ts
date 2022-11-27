dc4e152656fd1c69abaa002e3daaf56b31fa6c90
Ivan Semenov <ivsemenovdev@gmail.com>
2022-11-28 00:41:50 +0300

    feat: add popup to select location

--- src/app/blocks/header/components/header-top/components/header-top-right-col/header-top-right-col.component.ts
+++ src/app/blocks/header/components/header-top/components/header-top-right-col/header-top-right-col.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {MatDialog} from "@angular/material/dialog";
+import {DialogComponent} from "./components/dialog/dialog.component";
+
 
 @Component({
   selector: 'app-header-top-right-col',
   templateUrl: './header-top-right-col.component.html',
   styleUrls: ['./header-top-right-col.component.scss']
 })
-export class HeaderTopRightColComponent {
 
+export class HeaderTopRightColComponent {
+  constructor(public dialog: MatDialog) {}
+
+  openDialog() {
+    this.dialog.open(DialogComponent);
+  }
 }
+
