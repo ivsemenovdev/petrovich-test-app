b1e3524b26ff643567cdecdcbb5efe7883a9b745
Ivan Semenov <ivsemenovdev@gmail.com>
2022-12-13 18:24:59 +0300

    refactor: add grid tiles to footer component

--- src/app/blocks/footer/footer.component.ts
+++ src/app/blocks/footer/footer.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
+interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
+
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent {
+
+  tiles: Tile[] = [
+    {text: 'One', cols: 1, rows: 3, color: '#947fa1'},
+    {text: 'Two', cols: 1, rows: 3, color: '#947fa1'},
+    {text: 'Three', cols: 1, rows: 3, color: '#947fa1'},
+    {text: 'Four', cols: 1, rows: 3, color: '#947fa1'},
+    {text: 'Five', cols: 1, rows: 3, color: '#947fa1'},
+  ];
 
 }
