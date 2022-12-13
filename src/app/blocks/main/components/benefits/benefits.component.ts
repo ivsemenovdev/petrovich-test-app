0ce56ec38df6433de10130d18e0fb631d9245eae
Ivan Semenov <ivsemenovdev@gmail.com>
2022-12-13 21:48:18 +0300

    refactor: add dynamic data to a benefit component

--- src/app/blocks/main/components/benefits/benefits.component.ts
+++ src/app/blocks/main/components/benefits/benefits.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 interface Tile {
-  color: string;
   cols: number;
   rows: number;
   text: string;
+  img: string;
 }
 
 @Component({
@@ -15,10 +15,30 @@ interface Tile {
 export class BenefitsComponent {
 
   tiles: Tile[] = [
-    {text: 'One', cols: 3, rows: 1, color: '#947fa1'},
-    {text: 'Two', cols: 3, rows:1, color: '#947fa1'},
-    {text: 'Three', cols: 3, rows: 1, color: '#947fa1'},
-    {text: 'Four', cols: 3, rows: 1, color: '#947fa1'},
+    {
+      text: '<strong>Большой ассортимент</strong><br>строительных материалов всегда в наличии',
+      cols: 3,
+      rows: 1,
+      img: './assets/icons/benefit-first.svg'
+    },
+    {
+      text: '<strong>Доставка стройматериалов</strong><br>в течение двух часов или в любое удобное время',
+      cols: 3,
+      rows: 1,
+      img: './assets/icons/benefit-second.svg'
+    },
+    {
+      text: '<strong>Простой возврат товаров</strong><br>которые не пригодились в течение 360 дней',
+      cols: 3,
+      rows: 1,
+      img: './assets/icons/benefit-third.svg'
+    },
+    {
+      text: 'Многоуровневая <strong>система управления качеством</strong> по стандарту ISO 9001',
+      cols: 3,
+      rows: 1,
+      img: './assets/icons/benefit-four.svg'
+    },
   ];
 
 }
