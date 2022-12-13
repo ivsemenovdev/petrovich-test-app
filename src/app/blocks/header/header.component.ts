e36833fc45ff6f97f4d470a8195c3468272dc716
Ivan Semenov <ivsemenovdev@gmail.com>
2022-12-13 19:06:15 +0300

    feat: add sticky to header middle component

--- src/app/blocks/header/header.component.ts
+++ src/app/blocks/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,5 +7,11 @@ import { Component } from '@angular/core';
 })
 
 export class HeaderComponent {
+
+  headerMiddleFixed = false;
+
+  @HostListener('window:scroll') onScroll() {
+    this.headerMiddleFixed = window.scrollY > 48;
+  }
 
 }
