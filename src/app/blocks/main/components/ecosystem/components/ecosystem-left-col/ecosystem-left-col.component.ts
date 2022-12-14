d7b995d561b48bc69eb82976f124642b3f483e5d
Ivan Semenov <ivsemenovdev@gmail.com>
2022-12-15 00:50:49 +0300

    feat: add ecosystem left/right col component

---
+++ src/app/blocks/main/components/ecosystem/components/ecosystem-left-col/ecosystem-left-col.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-ecosystem-left-col',
+  templateUrl: './ecosystem-left-col.component.html',
+  styleUrls: ['./ecosystem-left-col.component.scss']
+})
+export class EcosystemLeftColComponent {
+
+
+}
---
+++ src/app/blocks/main/components/ecosystem/components/ecosystem-right-col/ecosystem-right-col.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ecosystem-right-col',
+  templateUrl: './ecosystem-right-col.component.html',
+  styleUrls: ['./ecosystem-right-col.component.scss']
+})
+export class EcosystemRightColComponent {
+
+}
